63b508e269c866aa25d65d9e628ebeb294b2a548
Petra Lignell <petralig@ety-a2123b-12.edu.wks.metropolia.fi>
2017-01-20 10:16:06 +0200

    update

---
+++ src/app/http-test/http-test.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import {Http, Response} from "@angular/http";
+
+@Component({
+  selector: 'app-http-test',
+  templateUrl: './http-test.component.html',
+  styleUrls: ['./http-test.component.scss']
+})
+export class HttpTestComponent implements OnInit {
+
+  private vastaus: any = {};
+  private apinVastaus: any = [];
+
+  constructor(private http: Http) { }
+
+  private getJson() {
+    this.http.get('tsconfig.json')
+      .subscribe(
+        (res:Response) => {
+          const json = res.json();
+          this.vastaus = json.compilerOptions;
+          console.log(this.vastaus);
+        }
+      );
+  }
+
+  private getApi() {
+    this.http.get('http://jkljobs.herokuapp.com/api/v1/jobs')
+      .subscribe(
+        (res:Response) => {
+          this.apinVastaus = res.json();
+          console.log(this.apinVastaus);
+        }
+      );
+  }
+
+  ngOnInit() {
+    this.getJson();
+    this.getApi();
+  }
+
+}
